cb805cda0536f104574ae5700396bf5bc97a8ad6
DaniMe98 <45035075+DaniMe98@users.noreply.github.com>
2019-03-23 21:03:03 +0100

    aggiunti commenti e return

--- Histogram/main.cpp
+++ Histogram/main.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 int main(){
 	
-	histogram h;
+	histogram h;  //creazione istogramma
 	h.create(); 
 	
-	h.add(0);
+	h.add(0);   //inserimento valori
 	h.add(1);
 	h.add(3);
 	h.add(0);
@@ -25,7 +25,7 @@ int main(){
 	
 	h.print();
 	
-	h.remove(1);  
+	h.remove(1);    //rimozione elementi
 	h.remove(3);
 	cout<<"Second"<<endl;
 	
@@ -33,4 +33,6 @@ int main(){
 	cout<<"La media e': "<<h.mean();
 	cout<<"La moda e' : "<<h.mode();
 	
+	return 0;
+
 }
